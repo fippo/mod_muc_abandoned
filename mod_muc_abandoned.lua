f46680e84c63326234a6c92e39b55442a7f371ee
Philipp Hancke <fippo@andyet.net>
2014-10-15 21:07:22 +0200

    keep participants in array so we can pick the oldest

--- mod_muc_abandoned.lua
+++ mod_muc_abandoned.lua
@@ -2,12 +2,33 @@
 -- LICENSE: MIT
 local serialization = require "util.serialization"
 
+local occupants = {}
+module:hook("muc-occupant-joined", function (event)
+        local room, nick, occupant = event.room, event.nick, event.occupant
+	if not occupants[room.jid] then occupants[room.jid] = {}; end
+	occupants[room.jid][#occupants[room.jid]+1] = event.nick
+end, 2)
+
 module:hook("muc-occupant-left", function (event)
         local room, nick, occupant = event.room, event.nick, event.occupant
+	if occupants[room.jid] then
+		for _, v in ipairs(occupants[room.jid]) do
+			if v == event.nick then
+				table.remove(occupants[room.jid], _)
+				if table.getn(occupants[room.jid]) == 0 then occupants[room.jid] = nil; end
+				break
+			end
+		end
+	end
 	if room:get_affiliation(occupant.bare_jid) == "owner" and room:get_persistent() == nil then
+		-- FIXME: only if this was the only owner
 		if room:has_occupant() then
-			local next_occupant = room:get_occupant_by_nick(next(room._occupants))
+			-- pick the oldest occupant
+			local next_occupant = room:get_occupant_by_nick(occupants[room.jid][1])
+			-- FIXME: occupant.bare_jid should no longer be necessary soon, bug is fixed in trunk,
+			-- just pass true
 			local ok, err = room:set_affiliation(occupant.bare_jid, next_occupant.bare_jid, "owner")
+			-- FIXME: remove old owner
 		end
 	end
 end, 2);
